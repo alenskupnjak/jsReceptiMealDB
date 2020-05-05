2a203bca3e2851ddfa51c7a77c4410a09d221773
AlenS <alenskupnjak@yahoo.com>
2020-05-05 10:19:08 +0200

    007 dev 1

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ const trazim = document.getElementById('trazim')
 
 
 function dohvatiPodatke() {
-  console.log('tutu');
-  console.log(trazim.value);
-  
-  console.log(event);
-
   // Pretrazujemo bazu prema upitu i vracamo podatke
   const podaci = podaciAPI.premaOpisuAPI(trazim.value)
   podaci.then(data =>{
--- podaci.js
+++ podaci.js
@@ -7,24 +7,17 @@ class PodaciApi {
   async premaOpisuAPI (data) {
     const podatak = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${data}`)
     .then(data => {
-      console.log(data);
       data = data.json();
-      console.log(data);
       return data
     })
     .then( data =>{
-      console.log('kaj sada');
-      
-      console.log(data);
        return data
     })
 
-    console.log(podatak);
     
 
 
 
     return podatak
-    
   }
 }
